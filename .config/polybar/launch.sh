11c8e94889b524189a47331bfe73f3eb93b3adda
FrozenArcher <yhy242989105@hotmail.com>
2022-07-15 17:05:06 +0000

    bar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,8 +5,13 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
+# wait until the processes have been shutdown
+#while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+#polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+polybar -q main
+#polybar -q info
 
 echo "Polybar launched..."
 
